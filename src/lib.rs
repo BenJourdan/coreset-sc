a45a55d59eb230ad9e02cbd553cf9b051b3e9e27
Ben Jourdan <ben@jourdan.me>
2024-10-13 17:24:26 +0000

    adding initial rust docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,19 @@
-// src/lib.rs
+//! coreset-sc is a Rust library with python bindings
+//! for Coreset Spectral Clustering.
+//!
+//! The library implementes the coreset spectral clustering algorithm given in the [paper](https://openreview.net/forum?id=1qgZXeMTTU).
+//!
+//! The (rust) API allows the user to pass as input one of the following:
+//!
+//! 1. A sparse symmetric adjacency matrix in the form of a CSR/CSC matrix.
+//!     - Coreset spectral clustering is performed on the input graph.
+//! 2. Input data points in euclidean space in the form of a 2D array.
+//!     - A k-nn graph is constructed using [faiss-rs](https://github.com/Enet4/faiss-rs)
+//!     - Or a kernel function can be specified and a coreset of the full similarity matrix can be computed lazily.
+//! 3. A k-nn oracle that can be used to query the k-nearest neighbours of a point.
+//!     - The coreset will be computed lazily using the oracle.
+
+
 
 mod rust;
 
